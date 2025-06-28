b65d4a865de54d01388e2860b418ffdacb0d4fcf
a71143055 <a71143055@gmail.com>
2025-06-28 19:27:02 +0900

    UP

---
+++ src/test/java/kr/ac/kopo/jeong/userproject/UserProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.jeong.userproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
